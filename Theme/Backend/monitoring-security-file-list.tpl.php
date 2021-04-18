dc4ddd9a5504ae0e8cccc4c4801f5154e0a42ad9
Dennis Eichhorn <spl1nes.com@googlemail.com>
2021-04-18 22:29:46 +0200

    new form layout and other tpl fixes

--- Theme/Backend/monitoring-security-file-list.tpl.php
+++ Theme/Backend/monitoring-security-file-list.tpl.php
@@ -26,7 +26,7 @@ echo $this->getData('nav')->render(); ?>
 
 <div class="row">
     <div class="box col-xs-12 wf-100">
-        <table class="default">
+        <table class="default sticky">
             <caption><?= $this->getHtml('Files'); ?><i class="fa fa-download floatRight download btn"></i></caption>
             <thead>
             <tr>
