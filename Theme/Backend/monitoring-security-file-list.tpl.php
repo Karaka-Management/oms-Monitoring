48478f8e8d647f8ad0c999c1bf0092185c9bab74
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2023-06-09 17:39:31 +0000

    continue implementation

--- Theme/Backend/monitoring-security-file-list.tpl.php
+++ Theme/Backend/monitoring-security-file-list.tpl.php
@@ -28,7 +28,7 @@ echo $this->data['nav']->render(); ?>
     <div class="box col-xs-12 wf-100">
         <div class="slider">
         <table class="default sticky">
-            <caption><?= $this->getHtml('Files'); ?><i class="fa fa-download floatRight download btn"></i></caption>
+            <caption><?= $this->getHtml('Files'); ?><i class="fa fa-download end-xs download btn"></i></caption>
             <thead>
             <tr>
                 <td class="wf-100"><?= $this->getHtml('File'); ?>
