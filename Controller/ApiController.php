fbb688fdac2ee257c602d00c70bbcc2157f75123
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2024-05-02 22:54:38 +0000

    Went through todos

--- Controller/ApiController.php
+++ Controller/ApiController.php
@@ -21,6 +21,12 @@ namespace Modules\Monitoring\Controller;
  * @license OMS License 2.0
  * @link    https://jingga.app
  * @since   1.0.0
+ *
+ * @feature Implement integrity check based on installed version and remote hash list (see monitoring-security.tpl.php)
+ *      https://github.com/Karaka-Management/oms-Monitoring/issues/3
+ *
+ * @feature If a user has all sub permissions instead of the parent permission with a wildcard for the child permissions.
+ *      https://github.com/Karaka-Management/oms-Monitoring/issues/6
  */
 final class ApiController extends Controller
 {
