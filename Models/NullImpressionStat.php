e03c7037124cae437d5fbf5311b9a007c247d576
Dennis Eichhorn <spl1nes.com@googlemail.com>
2024-05-19 04:18:07 +0200

    fix tests

---
+++ Models/NullImpressionStat.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Jingga
+ *
+ * PHP Version 8.2
+ *
+ * @package   Modules\Monitoring\Models
+ * @copyright Dennis Eichhorn
+ * @license   OMS License 2.0
+ * @version   1.0.0
+ * @link      https://jingga.app
+ */
+declare(strict_types=1);
+
+namespace Modules\Monitoring\Models;
+
+/**
+ * Impression stat class.
+ *
+ * @package Modules\Monitoring\Models
+ * @license OMS License 2.0
+ * @link    https://jingga.app
+ * @since   1.0.0
+ */
+final class NullImpressionStat extends ImpressionStat
+{
+    /**
+     * Constructor
+     *
+     * @param int $id Model id
+     *
+     * @since 1.0.0
+     */
+    public function __construct(int $id = 0)
+    {
+        $this->id = $id;
+    }
+}
